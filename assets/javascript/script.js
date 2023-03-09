5128a0f149bc5f63027ccd1d0b7241c7991fb81d
Chris Snyder <124528804+csnyder94@users.noreply.github.com>
2023-03-09 09:49:18 -0500

    added javascript comments

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,18 +6,18 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-//Textbox that displays generated password  
+// Textbox that displays generated password  
   passwordText.value = password;
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword(){
-//Pop-Up Window asking for password length
+// Pop-Up Window asking for password length
   var password = ''
   let passwordLength = window.prompt("How many characters would you like your password to contain?        Must be between 8 and 128 characters.") 
 
-//Pop-Up Window asking for confirmation of: special characters, numeric character, lowercase characters, and uppercase characters
+// Pop-Up Window asking for confirmation of: special characters, numeric character, lowercase characters, and uppercase characters
 if (passwordLength >= 8 && passwordLength <= 128){
 
   var specialCharacters = window.confirm("Click OK to confirm including special characters.");
@@ -32,14 +32,14 @@ if (passwordLength >= 8 && passwordLength <= 128){
   password = generatedPassword(generatedCharacters, passwordLength)
 }
 else {
-  window.alert("Password length must be at least 8 characters.")
+  window.alert("Password length must be between 8 and 128 characters.") // If password doesn't meet length criteria this alert appears. 
 }
 return password
 }  
 
 function generatedPassword (generatedCharacters, passwordLength){
   var password = ''
-
+// For loop to increment characters 1 at a time until the password length is reached.
 for (var i=0; i < passwordLength; i++){
   var random = Math.floor((Math.random() * generatedCharacters.length));
   password += generatedCharacters.charAt(random)
@@ -48,7 +48,7 @@ for (var i=0; i < passwordLength; i++){
 
 return password
 }
-
+// Calls randomCharacters to function.  The variables set the criteria for what letters the function can choose from.
 function randomCharacters(specialCharacters, numericCharacters, lowerCaseCharacters, upperCaseCharacters){
 
 var generatedSpecialCharacters = '`~!@#$%^&*()-_=+[{]}\|;:",<.>/?';
@@ -60,6 +60,7 @@ var generatedLowerCaseCharacters = 'abcdefghijklmnopqrstuvwxyz';
 var generatedUpperCaseCharcters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 var characters = ""
+// If statement to add generated characters from each specific class to the characters string if the specific class is true. If not selected to include, that classes characters will not be added.
 if (specialCharacters){
   characters += generatedSpecialCharacters
 }
